0bbbad77a937574545a0e5079f2afacfac61122b
tonyrud <tonrudny@gmail.com>
2025-10-18 08:59:18 -0400

    roll tracing examples

--- examples/elixir_phx/lib/elixir_phx_web/controllers/dice_controller.ex
+++ examples/elixir_phx/lib/elixir_phx_web/controllers/dice_controller.ex
@@ -1,22 +1,36 @@
 defmodule ElixirPhxWeb.DiceController do
   use ElixirPhxWeb, :controller
+  require OpenTelemetry.Tracer, as: Tracer
 
   require Logger
 
   def roll(conn, %{"sides" => sides}) do
     sides_int = String.to_integer(sides)
 
-    result = roll_dice(sides_int)
-    sleep_time = Enum.take_every(100..1500, 100) |> Enum.random()
+    # Add custom span with attributes
+    Tracer.with_span "dice.roll_with_sides" do
+      Tracer.set_attributes([
+        {"dice.sides", sides_int},
+        {"dice.type", "custom"}
+      ])
 
-    # Simulate some processing time
-    Process.sleep(sleep_time)
+      result = roll_dice(sides_int)
+      sleep_time = Enum.take_every(100..1500, 100) |> Enum.random()
 
-    conn
-    |> put_status(:ok)
-    |> json(%{result: result, sides: sides_int})
+      Tracer.set_attribute("dice.result", result)
+      Tracer.set_attribute("process.sleep", sleep_time)
+
+      # Simulate some processing time
+      Process.sleep(sleep_time)
+
+      conn
+      |> put_status(:ok)
+      |> json(%{result: result, sides: sides_int})
+    end
   rescue
     ArgumentError ->
+      Tracer.record_exception(%ArgumentError{message: "Invalid sides parameter"})
+
       conn
       |> put_status(:bad_request)
       |> json(%{error: "Invalid sides parameter"})
@@ -33,9 +47,22 @@ defmodule ElixirPhxWeb.DiceController do
   defp roll_dice(sides) when sides > 0 and sides < 30 do
     Logger.info("Rolling a #{sides}-sided dice")
 
-    result = Enum.random(1..sides)
+    Tracer.with_span "dice.generate_random" do
+      Tracer.set_attributes([
+        {"dice.sides", sides},
+        {"operation.type", "random_generation"}
+      ])
 
-    result
+      result = Enum.random(1..sides)
+
+      # Add some custom events
+      Tracer.add_event("dice.rolled", [
+        {"result", result},
+        {"timestamp", System.system_time(:millisecond)}
+      ])
+
+      result
+    end
   end
 
   defp roll_dice(_), do: raise(ArgumentError, "Sides must be greater than 0")

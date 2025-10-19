599ed83c795b990d2d0e1c914a062887cfef1440
tonyrud <tonrudny@gmail.com>
2025-10-19 07:45:06 -0400

    log message sh helper

--- examples/elixir_phx/run.sh
+++ examples/elixir_phx/run.sh
@@ -3,13 +3,19 @@
 # Exit on any error
 set -euo pipefail
 
-echo "Starting Elixir Phoenix OpenTelemetry example..."
+log_message() {
+  # Define ANSI escape codes for colors and reset
+  RED="\033[31m"
+  GREEN="\033[32m"
+  BLUE="\033[34m"
+  NC="\033[0m" # No Color - resets to default
+  local message=$@
+  printf "\n${GREEN}%s${NC}\n\n" "$message"
+}
 
 # Set environment variables for local development
 export OTEL_SERVICE_NAME="elixir-phx-dev"
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
-# export PHX_SERVER=true
-# export MIX_ENV=dev
 
 # Check if Docker is running
 #Open Docker, only if is not running
@@ -19,29 +25,28 @@ if (! docker stats --no-stream &>/dev/null ); then
  #Wait until Docker daemon is running and has completed initialisation
 while (! docker stats --no-stream &>/dev/null ); do
   # Docker takes a few seconds to initialize
-  echo "Waiting for Docker to launch..."
+  log_message "Waiting for Docker to launch..."
   sleep 1
 done
 fi
 
-
-echo "Starting PostgreSQL database using Docker..."
+log_message "Starting PostgreSQL database using Docker..."
 docker compose up db --remove-orphans -d
 
+# Check for otel stuff
+if (! docker ps --filter "name=lgtm" --filter "status=running" --quiet | grep -q .); then
+  log_message "OTLP Collector container is not running. Please start from the root directory: ./run-lgtm.sh"
+fi
+
 # Get dependencies and compile
-echo "Setting up the Elixir Phoenix application..."
+log_message "Setting up the Elixir Phoenix application..."
 mix setup
 
 
-echo ""
-echo ""
-echo "Starting Phoenix server with OpenTelemetry..."
-echo "Server will be available at: http://localhost:4000"
-echo "Dice roll: http://localhost:4000/rolldice"
-echo "Custom dice: http://localhost:4000/api/dice/20"
-echo ""
-echo "Make sure the LGTM stack is running (run 'run-lgtm.sh' from the root directory)"
-echo ""
+log_message "Starting Phoenix server with OpenTelemetry..."
+log_message "Server will be available at: http://localhost:4000"
+log_message "Dice roll: http://localhost:4000/rolldice"
+log_message "Custom dice: http://localhost:4000/api/dice/20"
 
 # Start the Phoenix server
 iex -S mix phx.server

b3889c613ed61c02df793b9386462d0e975566ed
tonyrud <tonrudny@gmail.com>
2026-02-28 17:43:55 -0500

    remove oats

--- examples/elixir_phx/README.md
+++ examples/elixir_phx/README.md
@@ -44,3 +44,4 @@ ab -n 100 -c 5 http://127.0.0.1:4000/rolldice
 - Connect logs to traces with links
 - Ecto examples with SQL statements
 - Examples with db.connection errors as well
+- Move to docker compose([example with full otel/lgtm and traffic generation](https://github.com/causely-oss/slow-query-lab/blob/main/docker-compose.yml))
--- generate-traffic.sh
+++ generate-traffic.sh
@@ -5,7 +5,7 @@ watch 'curl -s http://127.0.0.1:8080/rolldice; \
   curl -s http://127.0.0.1:8082/rolldice; \
   curl -s http://127.0.0.1:8083/rolldice; \
   curl -s http://127.0.0.1:8084/rolldice?rolls=5 \
-  curl -s http://127.0.0.1:4000/rolldice; \
+  curl -s http://127.0.0.1:4000/rolldice \
   curl -s http://127.0.0.1:4000/rolldice/12'
   
   
